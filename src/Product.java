bf2ce0101988a81ff042187e74ffcfa1e989fce1
pujamantri <pujamantri@users.noreply.github.com>
2014-08-05 14:25:22 +0530

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,32 +1,57 @@
+import java.math.BigDecimal;
 
 public class Product {
 	private String name;
 	private int quantity;
 	private Imported imported;
 	private Category category;
-	private double price;
+	private BigDecimal price;
+	private BigDecimal cost;
+	private BigDecimal tax;
 	
-	public Product(int quantity,Imported imported, String name, Category category, double price) {
+	public Product(int quantity,Imported imported, String name, Category category, BigDecimal price) {
 		this.quantity = quantity;
 		this.imported = imported;
 		this.name = name;
 		this.category = category;
 		this.price = price;
+		this.cost = new BigDecimal(0);
+		this.tax = new BigDecimal(0);
 	}
 	
 	public String getName() {
 		return name;
 	}
+	
 	public int getQuantity() {
 		return quantity;
 	}
+	
 	public Imported getImported() {
 		return imported;
 	}
+	
 	public Category getCategory() {
 		return category;
 	}
-	public double getPrice() {
+	
+	public BigDecimal getPrice() {
 		return price;
+	}
+
+	public BigDecimal getCost() {
+		return cost;
+	}
+
+	public void setCost(BigDecimal cost) {
+		this.cost = cost;
+	}
+
+	public BigDecimal getTax() {
+		return tax;
+	}
+
+	public void setTax(BigDecimal tax) {
+		this.tax = tax;
 	}
 }
