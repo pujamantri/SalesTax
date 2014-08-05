3307187402b4458a74f372ee2b75bcda8ade1ff4
pujamantri <pujamantri@users.noreply.github.com>
2014-08-05 14:22:34 +0530

    Update BillCalculator.java

--- src/BillCalculator.java
+++ src/BillCalculator.java
@@ -1,23 +1,17 @@
+import java.math.BigDecimal;
 import java.util.List;
-
 
 public class BillCalculator {
 	private Tax tax = new ImportDuty(new SalesTax());
+	private static BigDecimal HUNDRED = new BigDecimal(100);
 	
 	public void caclculateBill(ShoppingCart cart) {
 		List<Product> items = cart.getItems();
-		float totalTax = 0;
-		float totalBill = 0;
+	
 		for (Product eachItem : items) {
-			double taxAmount = tax.getTax(eachItem);
-			double price = Math.ceil(eachItem.getPrice() * eachItem.getQuantity() * 20) / 20;
-			double tax = Math.ceil(taxAmount * price * 20/ 100) / 20;
-			totalTax += tax;
-			totalBill += price + tax;
+			eachItem.setCost(eachItem.getPrice().multiply(new BigDecimal(eachItem.getQuantity())).setScale(2, BigDecimal.ROUND_HALF_UP));
+			eachItem.setTax(tax.getTax(eachItem).multiply(eachItem.getCost()).divide(HUNDRED).setScale(2, BigDecimal.ROUND_HALF_UP));
 		}
-		
-		cart.setTax(totalTax);
-		cart.setTotalCost(totalBill);
 	}
 	
 }
