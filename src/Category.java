2e2cc80df89189a7978605e21305aa53982caf4c
pujamantri <pujamantri@users.noreply.github.com>
2014-08-05 14:30:22 +0530

    Update Category.java

--- src/Category.java
+++ src/Category.java
@@ -1,29 +1,28 @@
-
 public enum Category {
 	BOOK {
 		@Override
-		public float getSalesTax() {
+		public double getSalesTax() {
 			return 0;
 		}
 	}, 
 	FOOD {
 		@Override
-		public float getSalesTax() {
+		public double getSalesTax() {
 			return 0;
 		}
 	},
 	MEDICAL_PRODUCTS {
 		@Override
-		public float getSalesTax() {
+		public double getSalesTax() {
 			return 0;
 		}
 	},
 	OTHERS {
 		@Override
-		public float getSalesTax() {
+		public double getSalesTax() {
 			return 10;
 		}
 	};
 	
-	abstract float getSalesTax();
+	abstract double getSalesTax();
 }
